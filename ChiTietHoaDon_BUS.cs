e19f11ccb9c8bd5a421d235846684be828e3d19d
TanTran3004 <45538871+TanTran3004@users.noreply.github.com>
2018-12-23 18:48:25 +0700

    Add files via upload

---
+++ ChiTietHoaDon_BUS.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DATA;
+using MODEL;
+using System.Data;
+namespace BUS
+{
+     public class ChiTietHoaDon_BUS
+    {
+         KetNoiSQL K = new KetNoiSQL();
+         ChiTietHoaDon_MODEL CTHD = new ChiTietHoaDon_MODEL();
+         public void Them_ChiTietHoaDon(ChiTietHoaDon_MODEL CTHD)
+         {
+             
+             string sql = "INSERT INTO CHI_TIET_HOA_DON VALUES('" + CTHD.MA_HOA_DON1 + "','" + CTHD.MA_SAN_PHAM1 + "','" + CTHD.KHOI_LUONG1 + "','" + CTHD.DON_GIA1 + "','" + CTHD.THANH_TIEN1 + "')";
+             K.Thao_Tac_Du_Lieu(sql);
+         }
+    }
+}
